9a04d76bb123d19ae4abde3097f1e13f4936d43e
kettler090@gmail.com <kettler090@gmail.com>
2024-11-27 19:35:44 +0700

    27.11. работает

---
+++ Pages/TM/TelemetryCombined.cs
@@ -0,0 +1,10 @@
+﻿using перенос_бд_на_Web.Models;
+
+namespace перенос_бд_на_Web.Pages.TM
+{
+    public class TelemetryCombined
+    {
+        public NedostovernayaTM Input { get; set; }
+        public NedostovernayaTM Experiment { get; set; }
+    }
+}
