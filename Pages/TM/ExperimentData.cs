f3088cac5c9c2431a7b6749757d09507a0868514
kettler090@gmail.com <kettler090@gmail.com>
2024-11-29 12:08:30 +0700

    дичь

---
+++ Pages/TM/ExperimentData.cs
@@ -0,0 +1,11 @@
+﻿namespace перенос_бд_на_Web.Pages.TM
+{
+    public class ExperimentData
+    {
+        public int IndexTm { get; set; }
+        public double CorrTm { get; set; }
+        public string Status { get; set; }
+        public double MaxLagranj { get; set; }
+        public double AvgLagranj { get; set; }
+    }
+}
