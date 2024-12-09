069c5f85a777d376f2843970899674f93296ba32
kettler090@gmail.com <kettler090@gmail.com>
2024-12-09 21:56:08 +0700

    dgggsef

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -9,6 +9,7 @@ namespace перенос_бд_на_Web.Models
         public DbSet<Slices> slices { get; set; }
         public DbSet <ActivePowerImbalance> active_power_imbalance { get; set; }
         public DbSet<ReactivePowerImbalance> reactive_power_imbalance { get; set; }
+        public DbSet<FilePath> file_paths { get; set; }
         //public DbSet <ExperimentFiles> experiment_file { get; set; }
         //public DbSet <ModifiedTMValues> tm_values_after_verification { get; set; }
 
---
+++ Models/FilePath.cs
@@ -0,0 +1,9 @@
+﻿namespace перенос_бд_на_Web.Models
+{
+    public class FilePath
+    {
+        public int id { get; set; } // Первичный ключ
+        public string path { get; set; } // Путь до файла
+        public DateTime updatedAt { get; set; } // Дата и время обновления
+    }
+}
