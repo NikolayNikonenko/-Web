13ce482c8b3a342da080418a6a6d5760c05cf350
kettler090@gmail.com <kettler090@gmail.com>
2024-12-15 17:46:11 +0700

    графики

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,4 @@
-﻿
-let chartInstances = new Map();
+﻿let chartInstances = new Map();
 
 function drawDonutChart(chartElement, successfulCount, totalCount) {
     console.log('Вызов функции drawDonutChart с параметрами: successfulCount =', successfulCount, ', totalCount =', totalCount);
@@ -20,6 +19,14 @@ function drawDonutChart(chartElement, successfulCount, totalCount) {
         chartInstances.get(chartElement).destroy();
     }
 
+    // Установка фиксированных размеров для канваса через JS (если необходимо)
+    //chartElement.width = chartElement.parentElement.offsetWidth; // Ширина зависит от родительского контейнера
+    //chartElement.height = chartElement.parentElement.offsetHeight; // Высота зависит от родительского контейнера
+
+    chartElement.width = 280; // Ширина зависит от родительского контейнера
+    chartElement.height = 280; // Высота зависит от родительского контейнера
+
+
     // Создать новую диаграмму и сохранить её
     const newChartInstance = new Chart(ctx, {
         type: 'doughnut',
@@ -32,8 +39,22 @@ function drawDonutChart(chartElement, successfulCount, totalCount) {
             }]
         },
         options: {
-            responsive: true,
-            maintainAspectRatio: false
+            responsive: false,
+            maintainAspectRatio: true,
+            layout: {
+                padding: {
+                    top: 20, // Добавьте отступ сверху
+                    bottom: 20, // Добавьте отступ снизу
+                    left: 20, // Отступ слева
+                    right: 20 // Отступ справа
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'top', // Позиция легенды
+                    align: 'center' // Выравнивание легенды по центру
+                }
+            }
         }
     });
 
