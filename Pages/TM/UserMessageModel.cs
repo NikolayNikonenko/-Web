1a70f63b9d21842e7acdcd0ad993883bca13d560
kettler090@gmail.com <kettler090@gmail.com>
2024-12-23 20:21:52 +0700

    вроде рещил outofmemory снова

---
+++ Pages/TM/UserMessageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace перенос_бд_на_Web.Pages.TM
+{
+    public class UserMessageModel
+    {
+        public string Text { get; set; }
+        public string Type { get; set; } // info, warning, error
+    }
+}
