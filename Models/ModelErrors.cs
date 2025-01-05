992f4061db4e309182c59a90706926813996fd12
kettler090@gmail.com <kettler090@gmail.com>
2025-01-05 13:24:04 +0700

    доработал ошибки в модели

--- Models/ModelErrors.cs
+++ Models/ModelErrors.cs
@@ -5,5 +5,6 @@
         public Guid ID { get; set; }
         public double IndexTm { get; set; }
         public string ErrorType { get; set; }
+        public string error_status { get; set; } = "Ошибка";
     }
 }
